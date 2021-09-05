6b8e2c61270508be704bde3f9eb566554640ab63
Anoop Sasidharan <anoopas.as@gmail.com>
2021-09-05 07:24:39 -0400

    cors

--- UserGroupManage.Service/Startup.cs
+++ UserGroupManage.Service/Startup.cs
@@ -40,6 +40,15 @@ namespace UserGroupManage.Service
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserGroupManage.Service", Version = "v1" });
             });
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddCors(cors =>
+            {
+                cors.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyOrigin();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +62,8 @@ namespace UserGroupManage.Service
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors();
 
             app.UseRouting();
 
