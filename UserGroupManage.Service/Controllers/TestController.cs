dd31c90bb5587d5db21760cdea9270114a26589c
Anoop Sasidharan <anoopas.as@gmail.com>
2021-09-08 10:47:53 -0400

    add a test controller

---
+++ UserGroupManage.Service/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserGroupManage.Service.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController:ControllerBase
+    {
+        public ActionResult<string> Get()
+        {
+            return Ok("test");
+        }
+    }
+}
